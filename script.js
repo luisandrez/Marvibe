a45402dad14d4556ac666e90b15244b44b00ffd8
luisandrez <luis27barza@gmail.com>
2026-07-14 20:21:58 -0500

    Primer comit

---
+++ script.js
@@ -0,0 +1,21 @@
+async function reservarYPagar() {
+    const reserva = {
+        nombre: document.getElementById("nombre").value,
+        email: document.getElementById("email").value,
+        telefono: document.getElementById("telefono").value,
+        monto: 200000
+    };
+
+    const respuesta = await fetch("http://localhost:3000/crear-transaccion", {
+        method: "POST",
+        hearders: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(reserva)
+    });
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    alert("conexion con el servidor realizada correctamente.");
+
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Bienvenido a la API de Mar Vibe"
+    });
+});
+app.get("/", (req, res) => {
+    res.send("servidor de mar vibe funcionando correctamente.");
+});
+
+const PORT = 3000;
+
+
+
+
+
+app.post("/crear-link-pago", async (req, res) => {
+    try {
+        const { nombre, servicio, monto } = req.body;
+        const respuesta = await axios.post(
+            "https://production.wompi.co/v1/payment_links",
+            {
+                name: `Reserva Mar Vibe - ${nombre}`,
+                description: servicio,
+                single_use: true,
+                collect_shipping: false,
+                currency: "COP",
+                amount_in_cents: monto * 100
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.WOMPI_PRIVATE_KEY}`
+                }
+            }
+        );
+        const id = respuesta.data.data.id;
+        res.json({
+            ok: true,
+            url: `https://checkout.wompi.co/l/${id}`
+        });
+    } catch (error) {
+        console.log(error.response?.data || error.message);
+
+        res.status(500).json({
+            ok: false,
+            mensaje: "Error al crear el link de pago"
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciando en http://localhost:${PORT}`);
+});
+
